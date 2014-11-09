bddc96d18728edd2f11ffbb39c824e9dc1d0d3d1
Undin <arseniy1993@mail.ru>
2014-11-10 02:23:18 +0300

    implemented Algebra

--- ITMOPrelude/Algebra.hs
+++ ITMOPrelude/Algebra.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoImplicitPrelude, FlexibleInstances #-}
 module ITMOPredule.Algebra where
 
 -- Реализовать для всего,
@@ -20,4 +20,42 @@ class Monoid a => Group a where
 
 -- Инстансы писать сюда
 
+data Add a = Add a 
+data Mul a = Mul a
 
+instance Monoid (Add Nat) where
+	mempty = Add natZero
+	mappend (Add a) (Add b) = Add $ a +. b
+
+instance Monoid (Add Int) where
+	mempty = Add intZero
+	mappend (Add a) (Add b) = Add $ a .+. b
+
+instance Monoid (Add Rat) where
+	mempty = Add $ Rat intZero natOne
+	mappend (Add a) (Add b) = Add $ a %+ b
+
+instance Monoid (Mul Nat) where
+	mempty = Mul natOne
+	mappend (Mul a) (Mul b) = Mul $ a *. b
+
+instance Monoid (Mul Int) where
+	mempty = Mul intOne
+	mappend (Mul a) (Mul b) = Mul $ a .*. b
+
+instance Monoid (Mul Rat) where
+	mempty = Mul $ Rat intOne natOne
+	mappend (Mul a) (Mul b) = Mul $ a %* b 
+
+instance Monoid (List a) where
+	mempty = Nil
+	mappend = (++)
+
+instance Group (Add Int) where
+	ginv (Add a) = Add $ intNeg a
+
+instance Group (Add Rat) where
+	ginv (Add a) = Add $ ratNeg a
+
+instance Group (Mul Rat) where
+ 	ginv (Mul a) = Mul $ ratInv a

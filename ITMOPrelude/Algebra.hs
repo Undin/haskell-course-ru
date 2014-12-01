a4e8036df7b8b7fd2d15ea9b331ca4c4a0c77a34
Undin <arseniy1993@mail.ru>
2014-12-01 03:19:52 +0300

    create and use newtype. implemented some new insatnces

--- ITMOPrelude/Algebra.hs
+++ ITMOPrelude/Algebra.hs
@@ -20,42 +20,58 @@ class Monoid a => Group a where
 
 -- Инстансы писать сюда
 
-data Add a = Add a 
-data Mul a = Mul a
+newtype Sum a = Sum { getSum :: a } 
+newtype Product a = Product { getProduct :: a }
+newtype Any a = Any { getAny :: a}
+newtype All a = All { getAll :: a}
 
-instance Monoid (Add Nat) where
-	mempty = Add natZero
-	mappend (Add a) (Add b) = Add $ a +. b
+instance Monoid (Sum Nat) where
+	mempty = Sum natZero
+	mappend a b = Sum $ getSum a +. getSum b
 
-instance Monoid (Add Int) where
-	mempty = Add intZero
-	mappend (Add a) (Add b) = Add $ a .+. b
+instance Monoid (Sum Int) where
+	mempty = Sum intZero
+	mappend a b = Sum $ getSum a .+. getSum b
 
-instance Monoid (Add Rat) where
-	mempty = Add $ Rat intZero natOne
-	mappend (Add a) (Add b) = Add $ a %+ b
+instance Monoid (Sum Rat) where
+	mempty = Sum $ Rat intZero natOne
+	mappend a b = Sum $ getSum a %+ getSum b
 
-instance Monoid (Mul Nat) where
-	mempty = Mul natOne
-	mappend (Mul a) (Mul b) = Mul $ a *. b
+instance Monoid (Product Nat) where
+	mempty = Product natOne
+	mappend a b = Product $ getProduct a *. getProduct b
 
-instance Monoid (Mul Int) where
-	mempty = Mul intOne
-	mappend (Mul a) (Mul b) = Mul $ a .*. b
+instance Monoid (Product Int) where
+	mempty = Product intOne
+	mappend a b = Product $ getProduct a .*. getProduct b
 
-instance Monoid (Mul Rat) where
-	mempty = Mul $ Rat intOne natOne
-	mappend (Mul a) (Mul b) = Mul $ a %* b 
+instance Monoid (Product Rat) where
+	mempty = Product $ Rat intOne natOne
+	mappend a b = Product $ getProduct a %* getProduct b 
+
+instance Monoid (Any Bool) where
+	mempty = Any False
+	mappend (Any x) (Any y) = Any (x || y)
+
+instance Monoid (All Bool) where
+	mempty = All True
+	mappend (All x) (All y) = All (x && y)
+
+instance Monoid Tri where
+	mempty = EQ
+	mappend LT _ = LT
+	mappend EQ x = x
+	mappend GT _ = GT
 
 instance Monoid (List a) where
 	mempty = Nil
 	mappend = (++)
 
-instance Group (Add Int) where
-	ginv (Add a) = Add $ intNeg a
+instance Group (Sum Int) where
+	ginv a = Sum $ intNeg $ getSum a
 
-instance Group (Add Rat) where
-	ginv (Add a) = Add $ ratNeg a
+instance Group (Sum Rat) where
+	ginv a = Sum $ ratNeg $ getSum a
 
-instance Group (Mul Rat) where
- 	ginv (Mul a) = Mul $ ratInv a
+instance Group (Product Rat) where
+ 	ginv a = Product $ ratInv $ getProduct a
